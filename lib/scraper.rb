8abb5f911c8bd8da1022925c8e7081c58a2fa4a2
Nia Bert <nbert14@gmail.com>
2019-07-13 06:34:04 +0000

    added pry

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,34 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 class Scraper
+    main_url = 'https://www.nytimes.com/books/best-sellers/combined-print-and-e-book-fiction/'
+  
+  def scrape_main_page(main_url)
+    page = Nokogiri::HTML(open(main_url))
+    
+    books = []
+    
+    book_list = page.css('.css-12yzwg4')
+    
+    book_list.css('div.css-xe4cfy').each do |book| 
+    
+     title = book.css('.css-5pe77f').text
+     author = book.css('.css-1j7a9fx').text.gsub(/"by"/, "")
+     weeks_on = book.css('.css-1o26r9v').text
+     publisher = book.css('.css-heg334').text
+     
+     #link = book.css('.css-79elbk').class? .css-6mwynb -> .css-wq7ea0 -> href
+    end
+    book_hash = {:title => title, :author => author, :weeks_on => weeks_on, :publisher => publisher}
+    books << book_hash
+  end
+  
+  
+  def scrape_bn_deets(bn_url)
+    bn_url = page.css('.css-wq7ea0').attribute('href')
+    
+    bn_url.each do ||
 end
+
