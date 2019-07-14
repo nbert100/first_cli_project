c7094ad08a46dc0a31fd2f79a7eccd264470e793
Nia Bert <nbert14@gmail.com>
2019-07-14 01:55:32 +0000

    correcting variables

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,15 +13,15 @@ class Scraper
     
     
     
-    book_list = page.css('.css-12yzwg4')
-    
-    book_list.css('div.css-xe4cfy').each do |book| 
-    binding.pry
-    # title = book.css('.css-5pe77f').text
-    # author = book.css('.css-1j7a9fx').text.gsub(/"by"/, "")
-    # weeks_on = book.css('.css-1o26r9v').text
-    # publisher = book.css('.css-heg334').text
-     
+    book_container = page.css('.css-12yzwg4')
+    book_list = book_container.css('div.css-xe4cfy')
+    book_list.each do |book| 
+   
+    title = book.css('.css-5pe77f').text
+    author = book.css('.css-1j7a9fx').text.gsub("by ", "")
+    weeks_on = book.css('.css-1o26r9v').text
+    publisher = book.css('.css-heg334').text
+      # binding.pry
     end
   #   book_hash = {:title => title, :author => author, :weeks_on => weeks_on, :publisher => publisher}
   #   books << book_hash
