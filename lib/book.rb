ab3adf0ce13fd6a146037a074dafe1625b5b9265
Nia Bert <nbert14@gmail.com>
2019-07-14 21:34:44 +0000

    attempting to match variables from second level to object

--- lib/book.rb
+++ lib/book.rb
@@ -1,23 +1,30 @@
 class Book
   @@all = []
 
-   attr_accessor :title, :author, :weeks_on, :pages, :price, :blurb, :publisher, :book_link
+   attr_accessor :title, :author, :weeks_on, :pages, :price, :publisher, :book_link, :release_date
 
   def self.all
     @@all
   end
   
-  # def self.new_from_index_page(book)
-  #   self.new(
-  #     book.css('.css-5pe77f').text,
-  #     book.css('.css-1j7a9fx').text.gsub("by", ""),
-  #     book.css('.css-1o26r9v').text)
+  # def self.new_from_scrape_page(b)
+  #   self.new(title: b.css('.css-5pe77f').text,
+  #       author: b.css('.css-1j7a9fx').text.gsub("by ", ""),
+  #       weeks_on: b.css('.css-1o26r9v').text,
+  #       publisher: b.css('.css-heg334').text,)
   # end
 
   def initialize(attributes)
     attributes.each do |key, v|
       self.send("#{key.to_s}=", v)
     end
+  
+  
+  # def book_details(more_info)
+  #   more_info.each do |key, value|
+  #     self.send("#{key.to_s}=", v)
+  #   end
+  # end
     
     # @title = title
     # @author = author
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,25 +4,6 @@ require 'pry'
 
 class Scraper
  
-# def self.scrape_page
-#   main_url = 'https://www.amazon.com/Books/b/?ie=UTF8&node=549028&ref_=sv_b_5'
-#   page = Nokogiri::HTML(open(main_url))
-#   book_container = page.css('.a-carousel-viewport')
-#   book_list = book_container.css('.a-carousel-card.acswidget-carousel__card')
-#   book_list.each do |book_box| 
-#     binding.pry
-#     attributes = { 
-#       title: book_box.css('.a-link-normal').text.strip,
-#       author: book_box.css('.acs_product-metadata__contributors').text,
-#       current_price: book_box.css('.a-size-base.a-color-price.acs_product-price__buying').text.gsub(" ", "")
-#       book_link: book_box.css('.css-wq7ea0').attribute('href').value
-      
-#     }
-        
-#     book = Book.new(attributes)
-#   end
-# end
- 
   def self.scrape_page
     main_url = 'https://www.nytimes.com/books/best-sellers/hardcover-fiction'
     page = Nokogiri::HTML(open(main_url))
@@ -30,41 +11,50 @@ class Scraper
     book_list = book_container.css('div.css-xe4cfy')
     
     book_list.each do |book_box| 
-    
-      attributes = { 
+        attributes = { 
         title: book_box.css('.css-5pe77f').text,
         author: book_box.css('.css-1j7a9fx').text.gsub("by ", ""),
         weeks_on: book_box.css('.css-1o26r9v').text,
         publisher: book_box.css('.css-heg334').text,
-        #book_link: book_box.css('.css-wq7ea0').attribute('href').value
-      }
+        }
       #different path for website due to button
-        attributes[:book_link] = book_list.css('.css-6mwynb').css("li")[1].css("a").attribute('href').value
+       
+       attributes[:book_link] = book_list.css('.css-6mwynb').css("li")[1].css("a").attribute('href').value
+       book = Book.new(attributes, more_info = nil)
+#---- pry here works
+          book_url = book.book_link
+          book_detail_doc = Nokogiri::HTML(open(book_url))
         
-    book = Book.new(attributes)
-    
-    #second level scrape
-    
-    book_url = book.book_link
-    
-    book_detail_doc = Nokogiri::HTML(open(book_url))
-    bd_page = book_detail_doc.css('div.book-page')
-    book_pub_info = bd_page.css('.book-top-section').css('.ibc-pub-info')[0].text.strip
-     binding.pry
+          bd_page = book_detail_doc.css('div.book-page')
+          book_doc = bd_page.css('div#book-data.book-top-section')
+          
+          book_doc.each do |info| 
+            more_info = {
+              pages: info.css('.ibc-pub-info')[0].text.strip.split(",").pop
+              price: info.css('span#list-price-price').text.gsub("*","")
+              release_date: info.css('p.ibc-pub-info')[1].text.split(": ").pop
+            }
+          end
     end
-    
-  end  
-    
-    #level two scrape
-    
-# def scrape_book_details
-#   binding.pry
-#   # book_link = self.get_page.css('.css-wq7ea0').attribute('href').value
-#   book_page = Nokogiri::HTML(open(book_link))
-  
-#   # Book.new(attributes)
+  end
+end  
+   
+# def get_page
+#     main_url = 'https://www.nytimes.com/books/best-sellers/hardcover-fiction'
+#     page = Nokogiri::HTML(open(main_url))
 #   end
-  
-end
+
+#   def scrape_book_index
+#     self.get_page.css('.css-12yzwg4').css('div.css-xe4cfy')
+#   end
+
+#   def get_books
+    
+#     scrape_book_index.each do |b|
+#       Book.new_from_main_page(b)
+#       binding.pry
+#     end
+#   end
+
 
 
